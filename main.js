33e51155fa39485f8509790a8caf239c2600f822
Jack Brounstein <Jack.Brounstein@yahoo.com>
2016-03-07 14:34:01 -0800

    fixes bug, so it actually works now

--- main.js
+++ main.js
@@ -18,9 +18,6 @@ $(document).ready(function(){
 	  player = {};
 	  player.name = name;
 	  player.hand = [];
-	  player.addCard = function(card){
-	    player.hand.push(card);
-	  };
 	  return player;
 	}
 	//example:
@@ -33,19 +30,19 @@ $(document).ready(function(){
 	    game.players[0].hand.push(res)
 	  }, "json");
 
-	$.get("http://pokeapi.co/api/v1/pokemon/2/", function(res) {
+	$.get("http://pokeapi.co/api/v1/pokemon/4/", function(res) {
 	    game.players[1].hand.push(res)
+		showGame()
 	  }, "json");
 
 	function showGame(){
-		console.log(game)
+		console.log(game.players[0].hand, "My console log")
 		var html_str = "<h1>Player 1: " + game.players[0].name+ "</h1>"
 		html_str += "<h1>Player 2: " + game.players[1].name + "</h1>"
 		html_str += `<p>${game.players[0].name} chose ${game.players[0].hand[0].name}!</p>`
 		html_str += `<p>${game.players[1].name} chose ${game.players[1].hand[0].name}!</p>`
 		html_str += "<p>Nothing else happens!</p>"
-		$(document).html = html_str
+		$("body").html(html_str)
 	}
 
-	showGame()
 })
